3360bacb186b388ee65a0cb1459d11fb0c9b4e51
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-18 00:10:54 +0600

    Design Login and Register page

--- src/components/Login/Login/Login.jsx
+++ src/components/Login/Login/Login.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+
 const Login = () => {
   return (
     <>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content grid grid-cols-1 ">
-          <div className="card flex-shrink-0 w-full mx-auto max-w-sm shadow-2xl bg-base-100">   
+          <div className="card flex-shrink-0 w-full mx-auto max-w-sm shadow-2xl bg-base-100">
             <form className="card-body">
-            <h1 className="text-center text-2xl font-bold">Login</h1>
+              <h1 className="text-center text-2xl font-bold">Login</h1>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -33,6 +36,12 @@ const Login = () => {
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Login</button>
+              </div>
+              <p>
+                Are you new?<Link to={"/register"} className="text-blue-600">Register</Link>
+              </p>
+              <div className="text-center">
+                <button ><FaGoogle style={{height:25, width:25}}/></button>
               </div>
             </form>
           </div>
--- src/components/Login/Register/Register.jsx
+++ src/components/Login/Register/Register.jsx
@@ -1,9 +1,64 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>
-      register
-    </div>
+    <>
+      <div className="hero min-h-screen bg-base-200 w-full">
+        <div className="hero-content ">
+          <div className="card flex-shrink-0 w-full mx-auto shadow-2xl bg-base-100">
+            <form className="card-body">
+              <h1 className="text-center text-2xl font-bold">Register</h1>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Photo URL"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register</button>
+              </div>
+              <p>
+                You have account.<Link to={"/login"} className="text-blue-600">Login</Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
