558c7d548f26c3e51dca26dee4094dcfe1b697de
Zihanur Rahman <zihanur15-2188@diu.edu.bd>
2023-05-20 08:16:18 +0600

    Show user image and name after login

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../AuthProvider/AuthProvider";
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
-console.log(user)
+  console.log(user);
   const handleLogOut = () => {
     logOut()
       .then(() => {})
@@ -136,7 +136,7 @@ console.log(user)
                         d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                       />
                     </svg>
-                    <span className="badge badge-sm indicator-item">8</span>
+                    <span className="badge badge-sm indicator-item">2</span>
                   </div>
                 </label>
                 <div
@@ -144,7 +144,7 @@ console.log(user)
                   className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
                 >
                   <div className="card-body">
-                    <span className="font-bold text-lg">8 Items</span>
+                    <span className="font-bold text-lg">2 Items</span>
                     <span className="text-info">Subtotal: $999</span>
                     <div className="card-actions">
                       <button className="btn btn-primary btn-block">
@@ -155,9 +155,13 @@ console.log(user)
                 </div>
               </div>
               <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <label
+                  tabIndex={0}
+                  className="btn btn-ghost btn-circle avatar"
+                  title={user?.displayName}
+                >
                   <div className="w-10 rounded-full">
-                    <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    <img src={user?.photoURL} />
                   </div>
                 </label>
                 <ul
